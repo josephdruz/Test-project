af81a3c4abe9303c486b42a7f4bb76151564b912
roberto <josephdruz51@gmail.com>
2024-07-07 02:10:06 +0800

    backend laravel

---
+++ backend/laravel-ip-management/app/Models/IPAddress.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IPAddress extends Model
+{
+    use HasFactory;
+}
